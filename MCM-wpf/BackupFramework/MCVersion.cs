875c4b3f8ff61787a4d2c39c0e2b79f1ba0e60ae
Andreas Larsen <northcode.no@gmail.com>
2013-06-13 20:09:18 +0200

    giti fix

---
+++ MCM-wpf/BackupFramework/MCVersion.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MCM.BackupFramework
+{
+    class MCVersion
+    {
+        string name { get; set; }
+        string version { get; set; }
+    }
+}
