33019ce072946dc886ee8cccc20f555d50b3deee
Andreas Larsen <northcode.no@gmail.com>
2013-06-13 20:09:18 +0200

    giti fix

---
+++ MCM-wpf/BackupFramework/JarBackup.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MCM.BackupFramework
+{
+    class JarBackup : IBackup
+    {
+        string name { get; set; }
+        
+
+        public void Unpack()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Pack()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
