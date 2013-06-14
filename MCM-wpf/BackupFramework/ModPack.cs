08d30a5d064eec0f2f77454adc9b30e40f862dca
Andreas Larsen <northcode.no@gmail.com>
2013-06-14 02:22:27 +0200

    work

---
+++ MCM-wpf/BackupFramework/ModPack.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MCM.BackupFramework
+{
+    class ModPack
+    {
+        public string Name { get; set; }
+
+        public List<Mod> mods { get; set; }
+    }
+}
