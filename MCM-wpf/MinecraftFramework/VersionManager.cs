d8ba0cb625a9dcf979b0e8fa8282d6d2a9965476
Jenjen1324 <northcodedotno@gmail.com>
2013-06-15 21:17:58 +0200

    Added version parsing

--- MCM-wpf/MinecraftFramework/VersionManager.cs
+++ MCM-wpf/MinecraftFramework/VersionManager.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Newtonsoft.Json.Linq;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 
@@ -8,5 +10,21 @@ namespace MCM.MinecraftFramework
     public class VersionManager
     {
         public static List<MinecraftVersion> versions { get; set; }
+
+        public void convertFromJson(string json)
+        {
+            JObject obj = JObject.Parse(json);
+
+            foreach (JObject obj2 in obj["versions"].Children<JObject>())
+            {
+                String file = MinecraftData.VersionsPath + "\\" + (string)obj2["id"] + "\\" + (string)obj2["id"] + ".json";
+                if (File.Exists(file))
+                {
+                    MinecraftVersion mcv = new MinecraftVersion();
+                    mcv.convertFromJson(File.ReadAllText(file));
+                    versions.Add(mcv);
+                }
+            }
+        }
     }
 }
