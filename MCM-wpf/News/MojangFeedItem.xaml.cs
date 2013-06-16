612bf76e03d09d7f1fb3cf3c59e971b652261249
Andreas Larsen <northcode.no@gmail.com>
2013-06-17 00:26:07 +0200

    awsome mojang news feed stuff!

--- MCM-wpf/News/MojangFeedItem.xaml.cs
+++ MCM-wpf/News/MojangFeedItem.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Windows;
@@ -19,9 +20,28 @@ namespace MCM.News
     /// </summary>
     public partial class MojangFeedItem : UserControl
     {
+        public string Data { get; set; }
+
         public MojangFeedItem()
         {
             InitializeComponent();
+        }
+
+        public void Init()
+        {
+            webViewer.NavigateToString(Data);
+        }
+
+        private void ExpandData(object sender, RoutedEventArgs e)
+        {
+            Height = 310;
+            webViewer.Visibility = System.Windows.Visibility.Visible;
+        }
+
+        private void TitleText_Collapsed(object sender, RoutedEventArgs e)
+        {
+            Height = 55;
+            webViewer.Visibility = System.Windows.Visibility.Collapsed;
         }
     }
 }
