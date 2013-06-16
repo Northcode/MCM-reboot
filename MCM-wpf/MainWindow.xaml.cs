9964eea5c202f6680509020e71a8ac7f0afb997f
Jenjen1324 <northcodedotno@gmail.com>
2013-06-16 12:33:10 +0200

    Changed to Metro GUI

--- MCM-wpf/MainWindow.xaml.cs
+++ MCM-wpf/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using MCM.BackupFramework;
+﻿using MahApps.Metro.Controls;
+using MCM.BackupFramework;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -19,7 +20,7 @@ namespace MCM
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : MetroWindow
     {
         public MainWindow()
         {
