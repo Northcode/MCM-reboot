21aa84f357e4272473f8ca233fea11f37064b02e
Jenjen1324 <northcodedotno@gmail.com>
2013-06-17 22:29:18 +0200

    UserData stuff

---
+++ MCM-wpf/User/MinecraftUser.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MCM.User
+{
+    class MinecraftUser
+    {
+        public string username { get; set; }
+        public string displayname { get; set; }
+        public string password_enc { get; set; }
+
+        public MinecraftUser(string username, string displayname, string password_enc)
+        {
+            this.username = username;
+            this.displayname = displayname;
+            this.password_enc = password_enc;
+        }
+
+        public static string encryptPwd(string password)
+        {
+            return password;
+        }
+
+        public static string decryptPwd(string encryptedData)
+        {
+            return encryptedData;
+        }
+    }
+}
