7e17437e5ad6e2c667eedcb33d0d45407797cf42
Jenjen1324 <northcodedotno@gmail.com>
2013-12-02 19:08:13 +0100

    Started new session info

---
+++ MCM-wpf/MinecraftFramework/SessionInfoYggdrasil.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MCM.MinecraftFramework
+{
+    public class SessionInfoYggdrasil : SessionInfo
+    {
+        public string accessToken { get; private set; }
+        public string clientToken { get; private set; }
+
+        public static SessionInfo Connect(string Username, string Password)
+        {
+
+        }
+    }
+}
